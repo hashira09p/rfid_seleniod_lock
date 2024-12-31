fa3f34da68ca050cad43b97f6a5e28c6eba2a16e
Jerome <jeromeabelida19@gmail.com>
2024-12-31 15:59:10 +0700

    added: routes and controllers for testing of esp32 connecting into rails server

---
+++ app/controllers/rfids_controller.rb
@@ -0,0 +1,21 @@
+class RfidsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+  def index;end
+  def create
+    def create
+      uid = params[:uid]
+      if uid.present?
+        Rails.logger.info "Received UID: #{uid}"
+        render json: { message: "UID received successfully", uid: uid }, status: :ok
+      else
+        render json: { error: "UID is missing" }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def rfid_params
+    params.require(:uid)
+  end
+end
