e750db29eaf82ae004042a7f43f1ba117d569c75
jerome <jeromeabelida19@gmail.com>
2025-01-05 00:23:56 +0800

    edited: schema and table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_31_104121) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_02_170543) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "firstname"
     t.string "middlename"
     t.string "lastname"
-    t.string "uid"
     t.integer "academic_college", default: 0
     t.integer "role", default: 1
     t.string "email", default: "", null: false
@@ -23,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_31_104121) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "api_token"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
