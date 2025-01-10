855820f1436ba155bfb45515c36fd23496f3fcce
jerome <jeromeabelida19@gmail.com>
2025-01-11 00:51:14 +0800

    added: has_many cards in model user and also seperate the admin aplication controller to professor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  has_many :cards
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: { admin: 0, professor: 1 }
 end
