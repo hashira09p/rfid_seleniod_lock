75dd69e1494689b4be2a77a2823040f698e95053
jerome <jeromeabelida19@gmail.com>
2025-01-20 20:29:52 +0800

    added: validations and enum for card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,9 @@
 class Card < ApplicationRecord
   enum status: { inactive: 0, active: 1 }
+  enum uid_type: { card: 0, tag: 1 }
 
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  validates :uid, presence: true, uniqueness: true
+  validates :uid_type, presence: true
 end
