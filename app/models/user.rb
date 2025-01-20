29b5851e8d40bdaab08ca6934be6ea8a6e50568c
jerome <jeromeabelida19@gmail.com>
2025-01-20 20:29:12 +0800

    added: academic college for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: { admin: 0, professor: 1 }
+  enum academic_college: { CIE: 0, COE: 1, CLA: 2, COS: 3, CAFA: 4, CIT: 5 }
 end
