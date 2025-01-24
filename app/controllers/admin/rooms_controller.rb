236f59b0ec8e015dfe777cf7ae1f674dc5a31ee2
jerome <jeromeabelida19@gmail.com>
2025-01-24 23:32:31 +0800

    added: rooms controller

---
+++ app/controllers/admin/rooms_controller.rb
@@ -0,0 +1,26 @@
+class Admin::RoomsController < AdminApplicationController
+  before_action :set_params, only: [:create, :update]
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(set_params)
+
+    if @room.save
+      redirect_to rooms_path, notice: 'Room was successfully created.'
+    else
+      redirect_to new_room_path, alert: 'Room was failed to be created.'
+    end
+  end
+
+  private
+
+  def set_params
+    params.require(:room).permit(:room_status, :room_number)
+  end
+end
