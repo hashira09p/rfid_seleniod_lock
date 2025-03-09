6a6d579ef83fc30cb1602763d721262b04766fb2
jerome <jeromeabelida19@gmail.com>
2025-03-09 22:26:30 +0800

    modified: logic in room display

--- app/controllers/admin/room_display_controller.rb
+++ app/controllers/admin/room_display_controller.rb
@@ -2,19 +2,6 @@ class Admin::RoomDisplayController < AdminApplicationController
   before_action :authenticate_admin_user!
 
   def index
-    current_time = Time.current
-    current_day = current_time.wday
-    formatted_time = current_time.strftime('%I:%M %p')
-
     @rooms = Room.all.includes(:schedules)
-
-    @room_statuses = @rooms.map do |room|
-      is_occupied = room.schedules.where(
-        "day = ? AND start_time >= ? AND end_time <= ?",
-        current_day, formatted_time, formatted_time
-      ).exists?
-
-      { room_number: room, status: is_occupied ? "Occupied" : "Available" }
-    end
   end
 end
