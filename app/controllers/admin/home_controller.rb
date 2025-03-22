ba5ef691e2c01478d4f1c66a16628b434a481a36
Precious Daniella Mapa <preciousdaniellamapa@gmail.com>
2025-03-23 00:37:14 +0800

    added: add professor button

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -19,4 +19,36 @@ class Admin::HomeController < AdminApplicationController
     @users = @users.where(academic_college: params[:academic_college]) if params[:academic_college].present?
     @users = @users.where(role: params[:role]) if params[:role].present?
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.role = "professor"
+
+    surname = @user.lastname&.upcase || "DEFAULT"
+    generated_password = "#{surname}123!"
+
+    @user.password = generated_password
+    @user.password_confirmation = generated_password
+
+    if @user.save
+      flash[:notice] = "Professor #{@user.firstname} #{@user.lastname} created successfully! Password: #{generated_password}"
+      redirect_to home_index_path
+    else
+      flash[:alert] = "Failed to create professor: #{@user.errors.full_messages.join(', ')}"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :firstname, :middlename, :lastname,
+      :academic_college, :email, :password, :password_confirmation
+    )
+  end
 end
