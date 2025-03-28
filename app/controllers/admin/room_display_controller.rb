888f52e91a16bc597437b88f5249d18093e090ee
Precious Daniella Mapa <preciousdaniellamapa@gmail.com>
2025-03-29 00:12:18 +0800

    modified: clickable room dislay, showing the schedules and available time

--- app/controllers/admin/room_display_controller.rb
+++ app/controllers/admin/room_display_controller.rb
@@ -6,6 +6,13 @@ class Admin::RoomDisplayController < AdminApplicationController
     @room_statuses = fetch_room_statuses
   end
 
+  def show
+    @room = Room.find(params[:id])
+    @schedules = @room.schedules.includes(:user).order(:start_time)
+    @todays_schedules = @schedules.where(day: Date.today.wday) # Get schedules for today
+    @available_slots = calculate_available_time(@todays_schedules)
+  end
+
   def room_statuses
     render json: { "1": "time_in", "2": "time_out", "3": "vacant" }
   end
@@ -23,5 +30,39 @@ class Admin::RoomDisplayController < AdminApplicationController
       end
     end
     statuses
+  end
+
+  def calculate_available_time(schedules)
+    full_day_range = (6 * 60..21 * 60).step(1).to_a # 6:00 AM to 9:00 PM in minutes
+    booked_minutes = schedules.reject { |s| s.start_time == s.end_time } # Ignore zero-duration bookings
+                              .map { |s| (s.start_time.hour * 60 + s.start_time.min..s.end_time.hour * 60 + s.end_time.min).to_a }
+                              .flatten
+                              .uniq
+
+    available_minutes = full_day_range - booked_minutes
+    format_available_slots(available_minutes)
+  end
+
+  def format_available_slots(available_minutes)
+    return [] if available_minutes.empty?
+
+    time_ranges = []
+    start_time = available_minutes.first
+    prev_time = start_time
+
+    available_minutes.each_cons(2) do |curr, next_time|
+      if next_time - curr > 1 # If there is a gap
+        time_ranges << "#{format_time(start_time)} - #{format_time(prev_time)}"
+        start_time = next_time
+      end
+      prev_time = next_time
+    end
+
+    time_ranges << "#{format_time(start_time)} - #{format_time(prev_time)}"
+    time_ranges
+  end
+
+  def format_time(minutes)
+    Time.new(2000, 1, 1, minutes / 60, minutes % 60).strftime('%I:%M %p')
   end
 end
