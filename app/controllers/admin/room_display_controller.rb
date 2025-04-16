7b3f9c66fe6826b3586321a27cf096022e05a6cf
Precious Daniella Mapa <preciousdaniellamapa@gmail.com>
2025-04-16 21:03:16 +0800

    added: pagination for room time logs

--- app/controllers/admin/room_display_controller.rb
+++ app/controllers/admin/room_display_controller.rb
@@ -11,8 +11,11 @@ class Admin::RoomDisplayController < AdminApplicationController
     @schedules = @room.schedules.includes(:user).order(:start_time)
     @todays_schedules = @schedules.where(day: Date.today.wday) # Get schedules for today
     @available_slots = calculate_available_time(@todays_schedules)
-    @today_time_tracks = @room.time_tracks.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-                              .order(created_at: :asc).to_a
+    @today_time_tracks = @room.time_tracks
+                              .where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+                              .order(created_at: :asc)
+                              .page(params[:page])
+                              .per(10)
   end
 
   def room_statuses
