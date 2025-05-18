e91c926509c0be9b89281639428e02b76c100bf0
Precious Daniella Mapa <preciousdaniellamapa@gmail.com>
2025-05-18 16:12:37 +0800

    modified: displayed the current role of the user in the header

--- app/controllers/admin_application_controller.rb
+++ app/controllers/admin_application_controller.rb
@@ -1,3 +1,11 @@
 class AdminApplicationController < ActionController::Base
   before_action :authenticate_admin_user!, except: [:create, :update, :new, :card_scan]
+
+  before_action :set_role_user
+
+  private
+
+  def set_role_user
+    @role_user = current_admin_user&.role
+  end
 end
