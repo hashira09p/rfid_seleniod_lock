d8517c73a1d7b3e85cc8d808b6449afb4912848f
Precious Daniella Mapa <preciousdaniellamapa@gmail.com>
2025-06-17 18:24:45 +0800

    Fix migration to use PostgreSQL-compatible syntax

--- db/migrate/20250309145710_add_time_in_and_time_out_to_time_tracks.rb
+++ db/migrate/20250309145710_add_time_in_and_time_out_to_time_tracks.rb
@@ -1,6 +1,6 @@
 class AddTimeInAndTimeOutToTimeTracks < ActiveRecord::Migration[7.0]
   def change
-    add_column :time_tracks, :time_in, :string, default: -> { "TIME_FORMAT(NOW(), '%I:%M %p')" }
-    add_column :time_tracks, :time_out, :string, default: -> { "TIME_FORMAT(NOW(), '%I:%M %p')" }
+    add_column :time_tracks, :time_in, :string, default: nil
+    add_column :time_tracks, :time_out, :string, default: nil
   end
 end
